e673baacf6d07d3b12fcafc30f388450be8aab47
mdavie <marianahodniki1991@gmail.com>
2022-02-07 14:32:22 -0700

    resolver update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,30 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+    Query: {
+
+        //get a user by username
+        me: async (parent, args, context) => {
+
+            if(context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('books')
+            
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in')
+
+        },
+
+    },
+
+
+    
+};
+
+module.exports = resolvers;
