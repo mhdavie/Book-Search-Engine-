67d57e367678ad539f57a7633e1c0ffc785d97b0
mdavie <marianahodniki1991@gmail.com>
2022-02-09 20:45:31 -0700

    login user user controler

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -111,3 +111,28 @@ const resolvers = {
     );
     }
   },
+  
+//login user
+login: async (parent, { email, password }) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new AuthenticationError('No user with this email found!');
+  }
+
+  const correctPw = await user.isCorrectPassword(password);
+
+  if (!correctPw) {
+    throw new AuthenticationError('Incorrect password!');
+  }
+
+  const token = signToken(user);
+  return { token, user };
+},
+
+
+
+};
+
+
+module.exports = resolvers;
