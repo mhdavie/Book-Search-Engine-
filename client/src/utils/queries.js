b77ee4f9cb784e52ec5c12222f9b40c7f32df38b
mdavie <marianahodniki1991@gmail.com>
2022-02-11 19:57:37 -0700

    deployed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,19 @@
-import { gql } from '@apollo/client';
+import gql from "graphql-tag";
 
-export const QUERY_ME = gql`
-{
-me {
-_id
-username
-email
-savedBooks {
-bookId
-authors
-image
-description 
-title
-link
-}
-
-}
-
-}
-
-
-`
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        title
+        bookId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
